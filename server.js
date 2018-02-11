0280abd14cb4c809053a28dc0ed3bada66149fc1
Profir Alexandr <profiralex@gmail.com>
2018-02-11 13:26:50 +0200

    Setup start script and heroku port

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
 
+const port = process.env.PORT || 3000;
+
 const app = express();
 
 const maintenance = false;
@@ -53,6 +55,6 @@ app.get('/bad', (req, res) => {
   res.send({errorMessage: 'Unable to handle request'});
 });
 
-app.listen(3000, () => {
-  console.log('Server si up on port 3000');
+app.listen(port, () => {
+  console.log(`Server is up on port ${port}`);
 });
